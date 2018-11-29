617aed233c26a0138ed224a5e788e140f1236129
Konrad Adamczyk <konradadamczykk@gmail.com>
2018-11-29 08:47:19 +0100

    lab4: Add packet logger
    Dummy PACKET_IN logger, works like a charm.
    
    Signed-off-by: Konrad Adamczyk <konradadamczykk@gmail.com>

---
+++ src/main/java/pl/edu/agh/kt/SdnLabListener.java
@@ -0,0 +1,87 @@
+package pl.edu.agh.kt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.ArrayList;
+
+import org.sdnplatform.sync.IInconsistencyResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SdnLabListener implements IOFMessageListener, IFloodlightModule {
+
+	protected IFloodlightProviderService floodlightProvider;
+	protected static Logger Logger;
+
+	@Override
+	public String getName() {
+		return SdnLabListener.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> col = new ArrayList<Class<? extends IFloodlightService>>();
+		col.add(IFloodlightProviderService.class);
+		
+		return col;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		Logger = LoggerFactory.getLogger(SdnLabListener.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		Logger.info("***************** STARTUP Called - SDN Lab *****************");
+	}
+
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(
+			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		Logger.info("***************** New PACKET_IN *****************");
+		
+		return Command.CONTINUE;
+	}
+
+}
